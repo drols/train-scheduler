aa9d6fd03f86c0f55060459de85d853002056540
Alexander <alex.drolshagen@gmail.com>
2018-11-20 18:31:23 -0700

    choo-choo-choo-magoo

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCLtqpExsHH1ILpKaijI_e8YyvKcS7oldc",
+    authDomain: "train-schedular-26e39.firebaseapp.com",
+    databaseURL: "https://train-schedular-26e39.firebaseio.com",
+    projectId: "train-schedular-26e39",
+    storageBucket: "",
+    messagingSenderId: "911608397597"
+  };
+  firebase.initializeApp(config);
+
+
+  
