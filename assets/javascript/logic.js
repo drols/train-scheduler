d6bfdcf9c9c8fc3bca2b0dc0610f7aedf8396c48
Alexander <alex.drolshagen@gmail.com>
2019-04-12 13:45:35 -0600

    working firebase!

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,39 +1,80 @@
-$(document).ready(function() {
-
-
-
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCLtqpExsHH1ILpKaijI_e8YyvKcS7oldc",
-    authDomain: "train-schedular-26e39.firebaseapp.com",
-    databaseURL: "https://train-schedular-26e39.firebaseio.com",
-    projectId: "train-schedular-26e39",
-    storageBucket: "",
-    messagingSenderId: "911608397597"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCLtqpExsHH1ILpKaijI_e8YyvKcS7oldc",
+  authDomain: "train-schedular-26e39.firebaseapp.com",
+  databaseURL: "https://train-schedular-26e39.firebaseio.com",
+  projectId: "train-schedular-26e39",
+  storageBucket: "train-schedular-26e39.appspot.com",
+  messagingSenderId: "911608397597"
+};
+firebase.initializeApp(config);
 
 
-  // Global Variables
 
-  var trainForm = $("#train-form").val().trim();
-  var trainName = $("#train-name-input").val().trim();
-  var destination = $("#destination-input").val().trim();
+var trainDatabase = firebase.database();
+
+$(document).ready(function () {
+
+$("#submit-button").on("click", function (event) {
+  // console.log("the button is working");
+  event.preventDefault();
+
+  var train = $("#train-name-input").val().trim();
+  var trainDest = $("#destination-input").val().trim();
   var trainTime = $("#train-time-input").val().trim();
-  var frequency = $("#frequency-input").val().trim();
-  var submitBtn = $("#submit-button").val().trim();
+  var trainFreq = $("#frequency-input").val().trim();
 
-  var newRow = $("<tr>").append(
-    $("<td>").text(trainName),
-    $("<td>").text(destination),
-    $("<td>").text(frequency),
-    $("<td>").text(trainName),
-    $("<td>").text(tMinutesTillTrain)
+  trainDatabase.ref().push({
+    name: train,
+    destination: trainDest,
+    startTime: trainTime,
+    frequency: trainFreq
+  });
 
-  );
-  
-  function submitForm() {
-    $()
-  }
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#train-time-input").val("");
+  $("#frequency-input").val("");
 
 });
+
+trainDatabase.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
+  var train = childSnapshot.val().name;
+  var trainDest = childSnapshot.val().destination;
+  var trainTime = childSnapshot.val().startTime;
+  var trainFreq = childSnapshot.val().frequency;
+  console.log(trainTime);
+
+  var startTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+  var newTime = moment().diff(moment(startTimeConverted), "minutes");
+  var tRemainder = newTime % trainFreq;
+  var minAway = trainFreq - tRemainder;
+  var nextArrival = moment().add(minAway, "minutes").format("hh:mm A");
+  console.log(minAway);
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(train),
+    $("<td>").text(trainDest),
+    $("<td>").text(trainFreq),
+    $("<td>").text(trainTime),
+    $("<td>").text(minAway)
+  );
+
+
+  $("#current-schedule > tbody").append(newRow);
+});
+
+function currentTime() {
+  var sec = 1;
+  time = moment().format('HH:mm:ss');
+  searchTime = moment().format('HH:mm');
+  $('#clock').html(time);
+
+  t = setTimeout(function () {
+    currentTime();
+  }, sec * 1000);
+}
+currentTime();
+
+})
